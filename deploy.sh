a114346641b77e2939e51865d7de718f21715878
Doug Freed <dwfreed@mtu.edu>
2019-02-14 01:09:31 -0500

    Update to svn1112, add more targets, improve build

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,3 @@
-#filename:deploy.sh
 #!/bin/bash
 # Deploy binaries built with travis-ci to GitHub Pages,
 # where they can be accessed by OpenWrt opkg directly
@@ -7,11 +6,12 @@ git clone https://${USER}:${TOKEN}@github.com/${USER}/${REPO}.git --branch gh-pa
 cd gh-pages || exit 1
 git config user.name "Doug Freed"
 git config user.email "dwfreed@mtu.edu"
-mkdir -p ${OSVER}
+mkdir -p $OSVER
 pushd $OSVER
-cp -r $TRAVIS_BUILD_DIR/sdk/$SDK_DIR/bin/packages/* .
-rm -r */{luci,packages,routing,telephony}
 ARCH=$(basename $TRAVIS_BUILD_DIR/sdk/$SDK_DIR/bin/packages/*)
+rm ${ARCH}/base/*.ipk
+cp -r $TRAVIS_BUILD_DIR/sdk/$SDK_DIR/bin/packages/* .
+rm -r ${ARCH}/{luci,packages,routing,telephony}
 cat > ${ARCH}/index.html <<EOF
 <html><body><pre>
 echo "src/gz openwrt-vlmcsd http://${USER}.github.io/${REPO}/${OSVER}/${ARCH}/base" >> /etc/opkg.conf
@@ -34,7 +34,7 @@ EOF
 git add -A
 popd
 #git pull
-git commit -a -m "Deploy Travis build $TRAVIS_BUILD_NUMBER to gh-pages"
+git commit -a -m "Deploy Travis build $TRAVIS_BUILD_NUMBER for $OSVER $ARCH to gh-pages"
 #git push -fq origin gh-pages:gh-pages > /dev/null 2>&1 || exit 1
 git push origin gh-pages > /dev/null 2>&1 || exit 1 # so that the key does not leak to the logs in case of errors
 #git push -f origin gh-pages:gh-pages
