e1af57efa698a0e0e7700e3bb31619a862fcaf6b
Doug Freed <dwfreed@mtu.edu>
2020-03-08 21:10:14 -0400

    Fix bad sed and remove unneeded patch

--- install.sh
+++ install.sh
@@ -7,7 +7,6 @@ export FILE=$TRAVIS_BUILD_DIR/cache/$(basename $SDK_URL)
 tar xf $FILE
 SDK_DIR="openwrt-sdk-*"
 cd $TRAVIS_BUILD_DIR/sdk/$SDK_DIR
-patch -Np1 < $TRAVIS_BUILD_DIR/padding.patch
 "echo 'untrusted comment: usign key of Doug Freed' > key-build"
 echo "$SIGNING_KEY" >> key-build
 mkdir package/vlmcsd
