4d4e3eade2e68caa80c73fbfb528d7d38b536bec
Doug Freed <dwfreed@mtu.edu>
2022-09-05 08:44:44 +0000

    update_matrix: fixes and improvements

--- update_matrix.py
+++ update_matrix.py
@@ -6,6 +6,48 @@ import requests
 import sh
 import sh.contrib
 
+class OpenWrtVersionSort:
+    def __init__(self, tag):
+        ver, _, rc = tag.partition('-rc')
+        major, _, minor = ver.rpartition('.')
+        self.major = major
+        self.minor = int(minor)
+        if rc:
+            self.rc = int(rc)
+        else:
+            self.rc = None
+        pass
+    def __lt__(self, other):
+        return self.compare(self, other) < 0
+    def __gt__(self, other):
+        return self.compare(self, other) > 0
+    def __eq__(self, other):
+        return self.compare(self, other) == 0
+    def __le__(self, other):
+        return self.compare(self, other) <= 0
+    def __ge__(self, other):
+        return self.compare(self, other) >= 0
+    def __ne__(self, other):
+        return self.compare(self, other) != 0
+
+    @staticmethod
+    def compare(a, b):
+        if a.major > b.major:
+            return 1
+        elif a.major < b.major:
+            return -1
+        elif a.minor > b.minor:
+            return 1
+        elif a.minor < b.minor:
+            return -1
+        elif a.rc is None and b.rc is not None:
+            return 1
+        elif a.rc is not None and b.rc is None:
+            return -1
+        else:
+            return a.rc - b.rc
+
+
 dump_target_info = sh.Command('scripts/dump-target-info.pl')
 
 sh.contrib.git.fetch('--tags')
@@ -13,8 +55,8 @@ sh.contrib.git.fetch('--tags')
 git_tags = sh.contrib.git.tag().split('\n')
 git_tags = [tag[1:] for tag in git_tags if tag.startswith('v')]
 majors = sorted({'.'.join(tag.split('.')[:2]) for tag in git_tags})
-majors = majors[-2:]
-tags = [sorted([tag for tag in git_tags if major in tag])[-1] for major in majors]
+majors = majors[-3:]
+tags = [sorted([tag for tag in git_tags if major in tag], key=OpenWrtVersionSort)[-1] for major in majors]
 
 matrix = []
 for major, tag in zip(majors, tags):
@@ -25,27 +67,30 @@ for major, tag in zip(majors, tags):
             continue
 
         arch, _, targets_str = line.partition(' ')
-        target, _, __ = targets_str.partition(' ')
 
-        url = f'https://downloads.openwrt.org/releases/{tag}/targets/{target}/'
+        for target in targets_str.split(' '):
+            url = f'https://downloads.openwrt.org/releases/{tag}/targets/{target}/'
 
-        response = requests.get(url)
+            response = requests.get(url)
 
-        soup = BeautifulSoup(response.text, 'html.parser')
+            soup = BeautifulSoup(response.text, 'html.parser')
 
-        sdk = ''
-        for link in soup.find_all('a'):
-            if not link['href']:
-                continue
+            sdk = ''
+            for link in soup.find_all('a'):
+                if not link['href']:
+                    continue
 
-            if 'openwrt-sdk' not in link['href']:
-                continue
+                if 'openwrt-sdk' not in link['href']:
+                    continue
 
-            sdk = link['href']
-            break
+                sdk = link['href']
+                break
 
-        if not sdk:
-            print(f'SDK not found for {tag} {arch} {target}!')
+            if sdk:
+                break
+
+        else:
+            print(f'SDK not found for {tag} {arch} {targets_str}!')
             continue
 
         job = {
